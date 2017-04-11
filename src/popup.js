4601e34eee3f58fd3fb64dfb273528dd8a4d5937
Luke Childs <lukechilds123@gmail.com>
2017-04-11 18:34:04 +0700

    Don't overwrite new responses with slow old responses

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,23 @@
 const search = document.querySelector('.search');
 const results = document.querySelector('.results');
 
+const searchIndexes = {
+  current: 0,
+  used: 0
+};
+
 search.addEventListener('input', e => {
+  searchIndexes.current++;
+  const searchIndex = searchIndexes.current;
+
   fetch(`https://ac.cnstrc.com/autocomplete/${search.value}?autocomplete_key=CD06z4gVeqSXRiDL2ZNK`)
     .then(res => res.json())
     .then(res => {
       const packages = res.sections.packages;
-      if(packages.length === 0) {
+      if(packages.length === 0 || (searchIndex < searchIndexes.used)) {
         return;
       }
+      searchIndexes.used = searchIndex;
       results.innerHTML = packages.map(package => package.value).join('<br>');
     });
 });
