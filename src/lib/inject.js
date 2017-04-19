892e87852824ac8baceb06f8652b6613d4075689
Luke Childs <lukechilds123@gmail.com>
2017-04-20 00:42:01 +0700

    Don't let inject method fail when not ran from chrome extension
    This makes it way easier to test functionality in the browser while developing

--- src/lib/inject.js
+++ src/lib/inject.js
@@ -1,5 +1,10 @@
 /* global chrome */
+const isChromeExtension = window.chrome && chrome.runtime && chrome.runtime.id;
+
 const inject = packages => {
+  if(!isChromeExtension) {
+    return;
+  }
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, { action: 'inject', packages }, console.log);
   });
