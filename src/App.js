73f9c354f406d41c7062098aa60204f3e628b994
Luke Childs <lukechilds123@gmail.com>
2017-04-19 16:17:02 +0700

    Store state of selected packages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import PackageSearch from './PackageSearch';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      packages: []
+    };
+  }
+
+  handlePackageSelect = pkg => {
+    this.setState({
+      packages: [...this.state.packages, pkg]
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Syringe</h1>
-        <PackageSearch onSubmit={console.log} />
+        <PackageSearch onSubmit={this.handlePackageSelect} />
         <div className="results"></div>
         <div className="selected"></div>
       </div>
