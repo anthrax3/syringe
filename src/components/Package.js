c782e014107d77b3af6dc615037077dc6ecfa369
Luke Childs <lukechilds123@gmail.com>
2017-04-20 20:54:27 +0700

    Style package

--- src/components/Package.js
+++ src/components/Package.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Package.css';
 
 const Package = props => {
   const onCrossClick = () => props.onCrossClick(props.pkg);
 
   return (
     <div className="Package">
-      <span onClick={onCrossClick}>✕</span>
+      <span className="remove" onClick={onCrossClick}>✕</span>
       {props.pkg}
     </div>
   );
