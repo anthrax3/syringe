eabd3013d4ec0efc98ae3b4645cc732ea05db2b0
Luke Childs <lukechilds123@gmail.com>
2017-04-24 22:02:35 +0700

    Revert back to content script
    So injection is handled outside of popup context (otherwise it gets killed if the popup is closed)
    And also we get errors in main window console

---
+++ public/content.js
@@ -0,0 +1,42 @@
+const scriptId = 'syringe-script';
+
+const injectPackages = (packages, cb) => {
+
+  const dependencies = packages.reduce((deps, pkg) => {
+    deps[pkg] = 'latest';
+    return deps
+  }, {});
+
+  fetch('https://wzrd.in/multi', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ dependencies })
+  })
+    .then(res => res.json())
+    .then(res => {
+
+      const existingScript = document.getElementById(scriptId);
+      if(existingScript !== null) {
+          existingScript.remove();
+      }
+
+      const bundle = Object.keys(res).reduce((bundle, pkg) => bundle + res[pkg].bundle, '');
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.innerHTML = bundle;
+      document.body.appendChild(script);
+      cb(true);
+    })
+    .catch(() => cb(false));
+}
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if(msg.action === 'inject') {
+    injectPackages(msg.packages, sendResponse);
+  }
+
+  // Allows us to respond async
+  return true;
+});
--- src/lib/inject.js
+++ src/lib/inject.js
@@ -6,28 +6,9 @@ const inject = packages => {
     return;
   }
 
-  const dependencies = packages.reduce((deps, pkg) => {
-    deps[pkg] = 'latest';
-    return deps
-  }, {});
-
-  fetch('https://wzrd.in/multi', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ dependencies })
-  })
-    .then(res => res.json())
-    .then(res => {
-      const bundle = Object.keys(res).reduce((bundle, pkg) => bundle + res[pkg].bundle, '');
-      const code = `(() => {
-        const s = document.createElement('script');
-        s.innerHTML = '(' + (() => { ${bundle} }).toString() + ')()';
-        document.body.appendChild(s);
-      })();`;
-      chrome.tabs.executeScript({ code });
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'inject', packages }, console.log);
+  });
 };
 
 export default inject;
